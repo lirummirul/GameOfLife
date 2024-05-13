4dfd83591637b19adf0314e0428792f569f9e589
lirummirul <lada.yakovleva.2018@mail.ru>
2024-05-13 16:35:00 +0300

    Initial Commit

---
+++ GameOfLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Lambert Lani on 5/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
